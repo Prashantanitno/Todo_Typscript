b3437f79e4d2881d50c88b5cc4529f4cdefe7ef1
Prashantanitno <aditya555antino@gmail.com>
2024-01-05 09:40:06 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>hello</h1>
+    </>
   );
 }
 
 export default App;
+
+// -------------------------->  Example  tupple
+// ->  let role: [number:string]
+// role=[3,"hello"]
+
+// --------------------------->  Example Object
+// type Person={
+//   name:string,
+//   age:number;
+// }
+
+// let person:Person={
+//   name:"hello",
+//   age:3
+// }
+
+// --------> Example Union(can be assign both number or string)
+// let age: number | string;
+// age='string'      // both correct
+// age=4
+
+//  Any = unknown ;
+
+// let p: unknown;
+
+//  ------------function void --------------------------
+// if the function is return undefiened then using void 
+// let printName:(name:string)=> void 
+
+// if the function is return nothing then using never 
+// let printName:(name:string)=> never
+
+
+// -####------->  to extend the values or interface  <-----------
+
+
+
+
